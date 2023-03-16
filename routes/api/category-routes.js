53f5d1d15bf0cf4796154b981f1c9c49942b9a41
knoxjess <110634800+knoxjess@users.noreply.github.com>
2023-03-15 23:35:29 -0500

    missing code added

---
+++ routes/api/category-routes.js
@@ -0,0 +1,61 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+require('../../models');
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+  router.get('/', (req, res) => {
+    Category.findAll({
+      include: [Product],
+    })
+      .then((categories) => res.json(categories))
+      .catch((err) => res.status(500).json(err));
+  });  
+  // be sure to include its associated Products
+});
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [Product],
+  })
+    .then((category) => res.json(category))
+    .catch((err) => res.status(400).json(err));
+});
+  // be sure to include its associated Products
+
+
+router.post('/', (req, res) => {
+  // create a new category
+  Category.create(req.body)
+  .then((category) => res.status(200).json(category))
+  .catch((err) => res.status(400).json(err));
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((category) => res.status(200).json(category))
+  .catch((err) => res.status(400).json(err));
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((category) => res.status(200).json(category))
+    .catch((err) => res.status(400).json(err));
+});
+
+module.exports = router;
